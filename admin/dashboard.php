55927e44f614c09450970749d609ca4420bf7cd2
akosidonel <reydonelmartinez@gmail.com>
2023-10-15 18:05:47 +0800

    file updated

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,14 +8,43 @@ include('../dbconnection/dbconnection.php');
  }else {?>
 
 <?php $title = 'Dashboard'; include('../include/header.php');?>
-		<div class="container">
-		<div class="content">
-    <h3>Dashboard</h3>
 
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Program</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		<form>
+				<div class="form-group">
+					<label>Programs & Project</label>
+					<input type="text" class="form-control" id="" placeholder="Enter Programs & Project">
+				</div>
+		</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="container">
+<div class="content">
+    
+	<h4 class="mb-4" >Dashboard</h4>
 	
-
-				<a href='#' id="wew" target="_blank" onclick="window.open('add_user.php','pagename','resizable,height=1000,width=540'); return false;" class="btn btn-primary my-2"><i class="fa-solid fa-laptop-file"></i> Add Program</a>
-			<form name="form1" method="post" action="adminIndex.php">
+	<form name="form1" method="post">
+	
+		<a href='#' class="btn btn-primary my-2" data-toggle="modal" data-target="#exampleModal"><i class="fa-solid fa-laptop-file"></i> Add Program</a>
+	</form>
         <div class="table-responsive">
         <table id="example" class="display table-striped" style="width:100%; height: 100%;">
           <thead>		       
@@ -34,14 +63,13 @@ include('../dbconnection/dbconnection.php');
 							<td>2023</td>
 							<td>Ongoing</td>
 							<td>
-							<i class="fa-sharp fa-solid fa-pen-to-square fa-lg text-success"></i> | <i class="fa-sharp fa-solid fa-trash fa-lg text-danger"></i>
+							<i class="fa-sharp fa-solid fa-pen-to-square fa-lg text-primary"></i> | <i class="fa-sharp fa-solid fa-trash fa-lg text-danger"></i>
 							</td>
 						</tr>
 					
 					</tbody>
 					</table>
         </div>
-			</form>
 		</div>
 	</div>
 
