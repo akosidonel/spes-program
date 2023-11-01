7a3aa424d77616007767e4dfbf0bd21735811070
akosidonel <reydonelmartinez@gmail.com>
2023-11-01 15:11:21 +0800

    bug fixes on application file

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,6 +9,18 @@ include('../dbconnection/dbconnection.php');
 
 <?php $title = 'Dashboard'; include('../include/header.php');?>
 
+<?php 
+$deploymentCount = mysqli_query($conn, "SELECT COUNT(dep_status) as total FROM deployment_history WHERE dep_status = 1");
+$query = mysqli_fetch_array($deploymentCount);
+$total = $query['total'];
+?>
+
+<?php 
+$sql = mysqli_query($conn, "SELECT capacity FROM program WHERE status = 1");
+$result = mysqli_fetch_array($sql);
+$capacity = $result['capacity'];
+?>
+
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -45,9 +57,11 @@ include('../dbconnection/dbconnection.php');
 <div class="content">
     
 	<h4 class="mb-4" >Dashboard</h4>
+ 
+	<h5 class="mb-4">Slot Available:  <?php echo htmlentities($capacity-$total)?></h5>
 	
 	<form name="form1" method="post">
-		<a href='#' class="btn btn-primary my-2" data-toggle="modal" data-target="#exampleModal"><i class="fa-solid fa-laptop-file"></i> Add Program</a>
+		<a href='#' class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fa-solid fa-laptop-file"></i> Add Program</a>
 	</form>
         <div class="table-responsive">
         <table id="example" class="display table-striped" style="width:100%; height: 100%;">
--- spes/application.php
+++ spes/application.php
@@ -5,35 +5,70 @@ include('../dbconnection/dbconnection.php');
  if(!isset($_SESSION['ulogin'])){
    header('Location:index.php');
    exit();
- }else {?>
+ }else { $id = $_SESSION['ulogin'];?>
+
   
 <?php $title = 'Application'; include('../include/spesheader.php');?>
 
+<?php 
+$sql = mysqli_query($conn, "SELECT COUNT(dep_status) as total FROM deployment_history WHERE dep_status = 1");
+$query = mysqli_fetch_array($sql);
+$total = $query['total'];
+?>
 
 <hr>
 <div class="container">
   
 <div class="card mb-4">
-  <h5 class="card-header">Application</h5>
+  <h5 class="card-header">Bulletin board</h5>
   <div class="card-body">
-     <table class="table mt-2">
-     <tr>
-      <th>Program</th>
-      <th>Status</th>
-     </tr>
-      <tbody>
-      <tr>
-        <td> <p class="card-text">Special Program for Employment of Students (SPES) Batch YFD-2023AUG is now open.</p></td>
-        <td><a href="#" class="btn btn-primary">Apply</a></td>
-      </tr>
-      </tbody>
-     </table>
+        <?php
+        $result = mysqli_query($conn, "SELECT status, capacity FROM program");
+        if(mysqli_num_rows($result)){
+          foreach($result as $row){ 
+          if($row['status']==1){
+            if($total<=$row['capacity']){
+              $spesid = mysqli_query($conn, "SELECT dep_status FROM deployment_history WHERE spes_id = '$id' ");
+              $query = mysqli_fetch_array($spesid);  
+              $spesStat = $query['dep_status'];
+              if($spesStat==''){?>
+                <table class="table mt-2">
+                        <tr>
+                          <th>Program</th>
+                          <th></th>
+                        </tr>
+                          <tbody>
+                        <tr>
+                            <td> <p class="card-text">Special Program for Employment of Students (SPES) is now open.</p></td>
+                            <td><a href="#" class="btn btn-primary">Apply</a></td>
+                        </tr>
+                        </tbody>
+                  </table>
+            <?php } else if($spesStat==0) {?>
+                  <td>
+                    <p class="card-text text-secondary">Your application is now processing..</p>
+                  </td>
+            <?php } else if($spesStat==1) {?>
+                  <td>
+                    <p class="card-text text-primary">Your application is ready for deployment soon...</p>
+                  </td>
+            <?php } else if($spesStat==2) {?>
+                  <td>
+                    <p class="card-text text-success">You are deployed..</p>
+                  </td>
+            <?php } } else  {?>
+              <tr>
+                <td><p class="card-text">Special Program for Employment of Students (SPES) is ongoing. </p><span class="card-text text-danger">No slot is available at this moment</span></td>
+              </tr>
+            <?php  } } else {?>
+            <tr>
+              <td><p class="card-text text-danger">Special Program for Employment of Students (SPES) is not yet open! wait for further announcement on our peso official facebook page..</p></td>
+            </tr>
+          <?php } } }?>
   </div>
 </div>
-   <h5>History</h5>
-   <br>
-
-
+<h5>History</h5>
+<br>
    <div class="table-responsive-md">
   <table class="table">
   <thead class="bg-success">
