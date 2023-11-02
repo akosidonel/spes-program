9d04a8e9a3386426bb0699436ad5fa369589e0c5
akosidonel <reydonelmartinez@gmail.com>
2023-11-02 08:41:00 +0800

    improvement and bug fixes

--- spes/application.php
+++ spes/application.php
@@ -11,60 +11,38 @@ include('../dbconnection/dbconnection.php');
 <?php $title = 'Application'; include('../include/spesheader.php');?>
 
 <?php 
-$sql = mysqli_query($conn, "SELECT COUNT(dep_status) as total FROM deployment_history WHERE dep_status = 1");
+$sql = mysqli_query($conn, "SELECT COUNT(dep_status) as total FROM deployment_history WHERE dep_status = 1  ");
 $query = mysqli_fetch_array($sql);
 $total = $query['total'];
 ?>
-
 <hr>
 <div class="container">
   
 <div class="card mb-4">
-  <h5 class="card-header">Bulletin board</h5>
+  <h5 class="card-header">Bulletin board <?php echo htmlentities($total)?></h5>
   <div class="card-body">
-        <?php
-        $result = mysqli_query($conn, "SELECT status, capacity FROM program");
-        if(mysqli_num_rows($result)){
-          foreach($result as $row){ 
-          if($row['status']==1){
-            if($total<=$row['capacity']){
+      <?php 
+       $result = mysqli_query($conn, "SELECT status FROM program WHERE status=1");
+       if(mysqli_num_rows($result)){
+        foreach($result as $row){
               $spesid = mysqli_query($conn, "SELECT dep_status FROM deployment_history WHERE spes_id = '$id' ");
-              $query = mysqli_fetch_array($spesid);  
-              $spesStat = $query['dep_status'];
-              if($spesStat==''){?>
-                <table class="table mt-2">
-                        <tr>
-                          <th>Program</th>
-                          <th></th>
-                        </tr>
-                          <tbody>
-                        <tr>
-                            <td> <p class="card-text">Special Program for Employment of Students (SPES) is now open.</p></td>
-                            <td><a href="#" class="btn btn-primary">Apply</a></td>
-                        </tr>
-                        </tbody>
-                  </table>
-            <?php } else if($spesStat==0) {?>
-                  <td>
-                    <p class="card-text text-secondary">Your application is now processing..</p>
-                  </td>
-            <?php } else if($spesStat==1) {?>
-                  <td>
-                    <p class="card-text text-primary">Your application is ready for deployment soon...</p>
-                  </td>
-            <?php } else if($spesStat==2) {?>
-                  <td>
-                    <p class="card-text text-success">You are deployed..</p>
-                  </td>
-            <?php } } else  {?>
-              <tr>
-                <td><p class="card-text">Special Program for Employment of Students (SPES) is ongoing. </p><span class="card-text text-danger">No slot is available at this moment</span></td>
-              </tr>
-            <?php  } } else {?>
-            <tr>
-              <td><p class="card-text text-danger">Special Program for Employment of Students (SPES) is not yet open! wait for further announcement on our peso official facebook page..</p></td>
-            </tr>
-          <?php } } }?>
+              $spesStatQuery = mysqli_fetch_array($spesid);  
+              $spesStat = $spesStatQuery['dep_status'];
+              if($spesStat==''){?>  
+                          <p class="card-text">Special Program for Employment of Students (SPES) is now open.<a href="#" class="text-primary"> Click Here to apply</a></p>
+                <?php } elseif($spesStat==1) { ?>
+                          <p class="card-text text-secondary">Your application is now processing..</p>
+                <?php } elseif($spesStat==2) {?>
+                          <p class="card-text text-primary">Your application is has been approved...</p>
+                <?php } elseif($spesStat==3) {?>
+                          <p class="card-text text-success">Congratulations you are Hired..</p>
+                <?php } elseif($spesStat==4) {?>
+                          <p class="card-text text-danger">End contract..</p>
+                <?php } ?>
+
+                <?php } } else{?>
+                          <p class="card-text text-secondary">Special Program for Employment of Students (SPES) are now closed / not yet open! please wait for further announcement on our Official Facebook page..</p>
+        <?php }?>
   </div>
 </div>
 <h5>History</h5>
@@ -73,34 +51,32 @@ $total = $query['total'];
   <table class="table">
   <thead class="bg-success">
     <tr>
-      <th scope="col">Batch No.</th>
-      <th scope="col">Program</th>
-      <th scope="col">Year</th>
-      <th scope="col">Status</th>
+      <th >Batch No.</th>
+      <th >Program</th>
+      <th >Year</th>
+      <th >Status</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>YFD-2023AUG</td>
-      <td>Special Program for Employment of Students (SPES)</td>
-      <td>2023</td>
-      <td>Completed</td>
-    </tr>
-    <tr>
-      <td>YFD-2020JUNE</td>
-      <td>Special Program for Employment of Students (SPES)</td>
-      <td>2020</td>
-      <td>Completed</td>
-    </tr>
+    <?php 
+    $history = mysqli_query($conn, "SELECT d.dep_status,d.spes_id,d.batch_number,p.batch_number,p.year,p.program 
+    FROM deployment_history AS d JOIN program AS p ON d.batch_number=p.batch_number WHERE d.spes_id = '$id' AND d.dep_status = 4 ");
+    if(mysqli_num_rows($history)){
+      foreach($history as $rows){?>
+            <tr>
+                <td><?=$rows['batch_number']?></td>
+                <td><?=$rows['year']?></td>
+                <td><?=$rows['program']?></td>
+                <td><?=$rows['dep_status']?></td>
+            </tr>
+     <?php } } else {?>
+      <tr><td>No data</td></tr>
+     <?php } ?>
   </tbody>
   </table>
 </div>
-
 </div>
-
 <?php include('../include/spesfooter')?>
-  
 </body>
 </html>
-
 <?php }?>
